d7835a594c1fa0f3f5147012068e13aec3afb426
CodeButt3rs <tekrimon4ever@yandex.ru>
2023-11-12 17:36:51 +0500

    Add files via upload

---
+++ HelloScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hello world programmmmm
+        print("Hello, world!!!!!!11!!!!1!!!1!!");
+        //Debug.Log("AAAAAA");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
